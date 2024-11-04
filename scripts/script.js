718c1af6b88d729c9be6f2ae039cf062b0a00fea
Jonny Twist <jsatwist@hotmail.com>
2024-11-03 22:10:07 -0800

    implemeted loggout ability which I will eventually move to the profile instead of the nav also made some progress on the profile page

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,18 @@ function sayHello() {
     
 }
 //sayHello();
+
+
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+// This function is actually only ever called when the index.html page is first 
+// displayed which is pretty much the same thing as when the logout button is pushed but just for future reference
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+      }).catch((error) => {
+        // An error happened.
+      });
+}
