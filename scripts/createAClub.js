02566d76ccc573c98f9995f467b48a44882ad9d5
Jonny Twist <jsatwist@hotmail.com>
2024-11-08 17:39:42 -0800

    created path to create a club form made admin recognizable eddited success .html

--- scripts/createAClub.js
+++ scripts/createAClub.js
@@ -1,8 +1,11 @@
 function createClub() {
+    var user = firebase.auth().currentUser;
+    console.log(user)
     let clubName = document.getElementById("clubName").value;
     let clubDescription = document.getElementById("description").value;
-        db.collection("clubs").add({
+        db.collection("unofficialClubs").add({
             name: clubName,
+            admin: user.uid,
             description: clubDescription,
             members: []
             // optional?
@@ -11,3 +14,12 @@ function createClub() {
             window.location.href = "successful.html"; // redirect to the submission successful page
         })
 }
+
+// HERE JUST FOR REFERENCE
+// function stuff() {
+//     db.collection("hello").doc("world").collection("events").add({
+//         time: "10:40",
+//         AMOrPM: "am",
+//         location: "SW05"
+//     })
+// } 
