c1114f8226202d85e4518bc6cf067c8e5406ea71
Mykyta Bozhanov <jgjgjgf980v@gmail.com>
2024-11-25 14:40:25 -0800

    added button for map page and linked events on home page to map page

--- scripts/home.js
+++ scripts/home.js
@@ -143,7 +143,7 @@ function displayCardsDynamically(collection) {
                         newcard.querySelector(".eventName").addEventListener("click", () => {
                             location.href = "eachEvent.html?docID=" + eventCard.clubID + "&eventID=" + eventCard.ID;
                         });
-                        newcard.querySelector('.eventLocation').innerHTML += eventCard.location;
+                        newcard.querySelector('.eventLocation').innerHTML += eventCard.location;  
                         newcard.querySelector('.eventDate').innerHTML += date;
                         newcard.querySelector('.eventTime').innerHTML += time;
                         newcard.querySelector('.goingCheck').innerHTML += '<label id="' + eventCard.clubID + eventCard.ID + 'Label" for="going">' + eventCard.attendees.length + (eventCard.attendees.length == 1 ? ' person is' : ' people are') + ' going. Are you?</label><input id="' + eventCard.clubID + eventCard.ID + 'Check" type="checkbox" name="going" value="' + eventCard.clubID + '">'
